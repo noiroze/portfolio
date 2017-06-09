539fca3fee7119bc0f22d698730d22b4d970196f
erica <noiroze@users.noreply.github.com>
2017-06-09 08:21:36 -0700

    Add files via upload

--- portfolio/index.php
+++ portfolio/index.php
@@ -1,10 +1,20 @@
-<!doctype html>
+<!doctype  class="nav-no-js">
 <html lang="en">
 <?php include_once 'includes/head.php'; ?>
 
 <body>
 <?php
 include_once 'includes/header.php';
+?>
+
+<!-- Hero Image Text -->
+<section id="banner">
+    <h2>ERICA WALKER</h2>
+    <p>Web Designer & Developer</p>
+</section>
+
+
+<?php
 include_once 'includes/about.php';
 include_once 'includes/projects.php';
 include_once 'includes/services.php';
@@ -12,5 +22,8 @@ include_once 'includes/contact.php';
 include_once 'includes/footer.php';
 include_once 'includes/bottom_include.php';
 ?>
+
+
+
 </body>
 </html>
