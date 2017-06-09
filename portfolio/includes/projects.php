7f58daa98ed2258fd9c1714a234d2e93eb127262
erica <noiroze@users.noreply.github.com>
2017-06-09 08:48:41 -0700

    Create projects.php

--- portfolio/includes/projects.php
+++ portfolio/includes/projects.php
@@ -20,7 +20,7 @@
                                                    alt="profile picture"></a>
                 <h3><a href="http://aliawong.com">Alia Wong</a></h3>
                 <h4>Journalism</h4>
-                <p>Alia Wong is an award winning journalist for the publication, The Atlantic. She was looking to update her existing website with a more modern feel. We refreshed her site with a by taking a minimalistic approach, focusing on the content and using light uncluttered pages. She has a blog that features her latest published works.</p>
+                <p>Alia Wong is an award winning journalist for the publication, The Atlantic. She was looking to update her existing website with a more modern feel. We refreshed her site by taking a minimalistic approach, focusing on the content and using light uncluttered pages. She has a blog that features her latest published works.</p>
             </div>
             <div class="gallery_product col-lg-4 col-md-4 col-sm-4 filter hdpe">
                 <a href="https://rarespirals.com"><img src="assets/images/tourmalinesingle.jpg" class="img-responsive"
@@ -33,7 +33,7 @@
                 <a href="http://ujha.ericawalkerwebsites.com"><img src="assets/images/golden-palace.jpg" alt="mandala"></a>
                 <h3><a href="http://ujha.ericawalkerwebsites.com">Uma Joy Healing Art</a></h3>
                 <h4>Art and E-Commerce</h4>
-                <p>This project is still in progress, but nearing completion. Uma Joy Mulnick is an artist who not only sells her work online, but regularly holds workshops and events teaching others how to create art. She wanted to rebrand her business (and existing site), so we created an retail platform with a new domain as well. Once complete, visitors will be able to purchase art and reserve space for upcoming events.</p>
+                <p>This project is still in progress, but nearing completion. Uma Joy Mulnick is an artist who not only sells her work online, but regularly holds workshops and events teaching others how to create art. She wanted to rebrand her business (and existing site), so we created a retail platform with a new domain as well. Once complete, visitors will be able to purchase art and reserve space for upcoming events.</p>
             </div>
         </div>
     </div>
